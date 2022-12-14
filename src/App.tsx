26576fd000c581c14f61806cf47367fbb40002ba
Roberto Zanin <raszanin@gmail.com>
2022-12-14 11:18:01 -0300

    import font

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-import './styles/main.css';
+import './styles/global.css';
 
 export function App() {
-  return <h1>Ignite Lab - Design System</h1>;
+  return (
+    <>
+      <h1 className='text-violet-500'>Ignite Lab - Design System</h1>
+      <button className='bg-violet-500 font-medium m-2 px-4 py-2 rounded text-white hover:bg-violet-800'>
+        Entrar
+      </button>
+    </>
+  );
 }
