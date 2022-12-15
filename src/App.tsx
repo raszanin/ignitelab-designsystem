a1ace9be48b0da40659ee4a619caa5965783cdd0
Roberto Zanin <raszanin@gmail.com>
2022-12-15 11:12:41 -0300

    storybook components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 import './styles/global.css';
 
 export function App() {
-  return (
-    <>
-      <h1 className='text-violet-500'>Ignite Lab - Design System</h1>
-      <button className='bg-violet-500 font-medium m-2 px-4 py-2 rounded text-white hover:bg-violet-800'>
-        Entrar
-      </button>
-    </>
-  );
+  return <></>;
 }
